f791db549bce43444018a5b23d646248c1f3bd7c
Ajiket97 <ajitshingnare02@gmail.com>
2024-05-28 09:43:38 +0530

    the bank project

---
+++ src/main/java/com/Keyword/Locators.java
@@ -0,0 +1,19 @@
+package com.Keyword;
+
+public interface Locators {
+	String APP_URL = "https://parabank.parasoft.com/parabank/index.htm";
+	String NAME = "//input[@name=\"customer.firstName\"]";
+	String REGISTER = "//a[text()='Register']";
+	String LASTNAME="//input[@name=\"customer.lastName\"]";
+	String ADDRESS="//input[@name=\"customer.address.street\"]";
+	String CITY="//input[@name=\"customer.address.city\"]";
+	String STATE ="//input[@name=\"customer.address.state\"]";
+	String ZIPCODE ="//input[@name=\"customer.address.zipCode\"]";
+	String MOBILENO="//input[@name=\"customer.phoneNumber\"]";
+	String SSN="//input[@name=\"customer.ssn\"]";
+	String USERNAME ="//input[@name=\"customer.username\"]";
+	String PASS="//input[@name=\"customer.password\"]";
+	String CONFIRM_PASS="//input[@name=\"repeatedPassword\"]";
+	String ENTER = "//input[@value=\"Register\"]";
+	String TEXT = "//span[@id=\"customer.firstName.errors\"]";
+}
---
+++ src/test/java/com/LoginPage/VerifyName.java
@@ -0,0 +1,40 @@
+package com.LoginPage;
+
+import org.testng.annotations.Test;
+
+import com.Keyword.Keywords;
+import com.Keyword.Locators;
+
+public class VerifyName {
+	@Test
+	public void verifyNameRequriedOrNot()  {
+		Keywords key = new Keywords();
+		
+		String msg = "First name is required.";
+		key.openBrowser();
+		key.launchUrl(Locators.APP_URL);
+		key.clickButton(Locators.REGISTER);
+		//key.sendKeys(Locators.NAME, "Smita");
+		key.sendKeys(Locators.LASTNAME, "pawar");
+		key.sendKeys(Locators.ADDRESS, "pune");
+		key.sendKeys(Locators.CITY, "pune");
+		key.sendKeys(Locators.STATE, "jj");
+		key.sendKeys(Locators.ZIPCODE, "555");
+		key.sendKeys(Locators.MOBILENO, "9876543210");
+		key.sendKeys(Locators.SSN, "sdjks");
+		key.sendKeys(Locators.USERNAME, "ajit");
+		key.sendKeys(Locators.PASS, "Aj@123");
+		key.sendKeys(Locators.CONFIRM_PASS, "Aj@123");
+		key.clickButton("//input[@value=\"Register\"]");
+
+		//key.clickButton(Locators.REGISTER);
+		String msg1 = key.getText(Locators.TEXT);
+		if (msg.equals(msg1)) {
+			System.err.println("name is requred ");
+		} else {
+System.out.println("not requried");
+		}
+
+	}
+
+}
